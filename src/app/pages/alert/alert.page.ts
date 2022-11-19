787985a1472bef15259fec9988a81a2aa090277b
Luis Fernando Mendez Barrera <luis.fernando.mendez.barrera2708@gmail.com>
2022-11-19 13:12:48 -0600

    avatar y alerts

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
+  async presentAlert() {
+    const alert = await this.alertCtrl.create(
+      {
+     backdropDismiss:false,
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertCtrl.create(
+      {
+     backdropDismiss:false,
+      header: 'Alert',
+      subHeader: 'Important message',
+      message: 'This is an alert!',
+      buttons: [
+        {
+        text:'Ok',
+        handler:()=>{
+          console.log('Click en ok!');
+        }
+        },
+        {
+          text:'Cancelar',
+          /*handler:()=>{
+            console.log('Click en cancelar');
+          }*/
+          role:'cancel',
+          cssClass:'red'
+          }
+    ],
+    });
+    await alert.present();
+  }
+  async presentAlertPrompt() {
+    const alert = await this.alertCtrl.create({
+      header: 'Please enter your info',
+      buttons: ['OK'],
+      inputs: [
+        {
+          placeholder: 'Name',
+        },
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
 
-}
+    await alert.present();
+  }
+  }
