14a845c715907097f0a6a4a9aef9251fcec0b22f
Luis Fernando Mendez Barrera <luis.fernando.mendez.barrera2708@gmail.com>
2022-11-26 13:34:33 -0600

    pages,buttons y card

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -26,8 +26,22 @@ export class InicioPage implements OnInit {
     icon: 'beaker-outline',
     name: 'Avatar',
     redirectTo: '/avatar'
+  },
+  {
+    icon: 'radio-button-off-outline',
+    name: 'Button',
+    redirectTo: '/button'
+  },
+  {
+    icon: 'card-outline',
+    name: 'Cards',
+    redirectTo: '/card'
+  },
+  {
+    icon: 'checkmark-circle-outline',
+    name: 'Checks',
+    redirectTo: '/check'
   }
-
   ];
 
   constructor() { }
