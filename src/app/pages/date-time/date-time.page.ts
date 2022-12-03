c2882eb74adde66da861fef11894ecf82b0bcbac
Luis Fernando Mendez Barrera <luis.fernando.mendez.barrera2708@gmail.com>
2022-12-03 13:13:41 -0600

    date picker

---
+++ src/app/pages/date-time/date-time.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-date-time',
+  templateUrl: './date-time.page.html',
+  styleUrls: ['./date-time.page.scss'],
+})
+export class DateTimePage implements OnInit {
+
+  fechaNaci: Date = new Date();
+
+  customPickerOptions = {
+    buttons: [{
+      text: 'Hola'
+    },
+    {
+      text: 'Mundo'
+    }
+    ]
+  };
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  cambioFecha(event) {
+    console.log(event);
+    console.log(new Date(event.detail.value));
+  }
+
+}
